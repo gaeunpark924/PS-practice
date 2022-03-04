eba6eb841c34137e6b5fd6e219ddee1e5c7e6384
Gaeun Park <51811995+gaeunpark924@users.noreply.github.com>
2022-03-04 23:16:58 +0900

    Update 소수구하기(수학)_일곱난쟁이(브루트포스).py

--- 3월1주차_DP/소수구하기(수학)_일곱난쟁이(브루트포스).py
+++ 3월1주차_DP/소수구하기(수학)_일곱난쟁이(브루트포스).py
@@ -1,9 +1,8 @@
-
 #백준 2309번 일곱 난쟁이
 #https://www.acmicpc.net/problem/2309
-#오랜만에 combinations 사용해서 풀었다.
 #입력 받는 수의 범위가 9 로 정해져 있어 combinations를 이용해서 9 명의 난쟁이 중에 7명의 난쟁이를 뽑는 모든 조합을 만들어서 풀었다.
 #7명의 난쟁이의 키의 합이 100이면 키를 출력하고 break해서 더이상 탐색하지 않고 종료한다.
+#combinations을 사용한 문제는 오랜만에 풀어보는데 잊어버리지 않도록 종종 풀어야 겠다!
 from itertools import combinations
 height = [int(input()) for _ in range(9)]
 height = sorted(height)
@@ -14,6 +13,7 @@ for value in combinations(height,7):
             print(value[i])
         break
 
+        
 #백준 1929번 소수 구하기
 #https://www.acmicpc.net/problem/1929
 #처음 푼 풀이
