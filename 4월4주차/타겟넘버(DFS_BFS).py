ebd3b7f1e830f152122026cc634e607da0567b20
Gaeun Park <qkrrkdms924@gmail.com>
2022-04-19 22:49:07 +0900

    Update 타겟넘버(DFS_BFS).py

--- 4월4주차/타겟넘버(DFS_BFS).py
+++ 4월4주차/타겟넘버(DFS_BFS).py
@@ -2,8 +2,8 @@
 #https://programmers.co.kr/learn/courses/30/lessons/43165
 #오랜만에 그래프 문제를 풀어서 좀 오래걸렸던 것 같다
 #깊이 우선 탐색 DFS 알고리즘을 사용했고 재귀로 풀었다.
+#pop이나 popleft를 사용하지 않고 데이터 위치(인덱스)를 파라미터로 넘겨주었다.
 #answer를 전역변수로 만들었는데, 재귀를 쓸 때 전역변수를 사용하는 것이 좋은 풀이인지는 모르겠다.
-#그리고 pop이나 popleft를 사용하지 않고 데이터 위치(인덱스)를 파라미터로 넘겨주었다.
 answer = 0
 def dfs_target_number(idx, result, n, numbers, target):
     global answer
